49b073803b429bcb05cac8b8d5675a3d5a708868
Kiinyo <iiyoheki@gmail.com>
2021-02-09 11:55:01 -0700

    added aeiou()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+pub fn aeiou () -> String {
+    ("aeiou").to_string()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn aeiou_test() {
+        assert_eq!(crate::aeiou(), "aeiou");
     }
 }
